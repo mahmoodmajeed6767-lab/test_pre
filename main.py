9e437e1965ca17f69c35f74a6a186533fca3e422
Awais Majeed <awais@gmail.com>
2026-04-30 23:49:50 +0500

    this is my first quiz

---
+++ main.py
@@ -0,0 +1,99 @@
+####this is the classification on the age
+
+# age = int(input("Enter your age : "))
+# if age < 2:
+#     catagory = "infant"
+# elif age < 12:
+#     catagory = "child"
+# elif age < 20:
+#     catagory = "teenage"
+# elif age < 65:
+#     catagory = "young"
+# else:
+#     catagory = "senior"
+# print(f"this is a {catagory}")
+
+    
+#######this is the grading system
+# age = int(input("Enter your age : "))
+# if age >= 90:
+#     grade = "A"
+# elif age >= 80:
+#     grade = "B"
+# elif age >= 70:
+#     grade = "C"
+# elif age >= 60:
+#     grade = "D"
+# else:
+#     grade = "Fail"
+# print(f"This is the grade of students : {grade}")
+
+####Discount Calculator
+# Total_purchase = float(input("Enter your toatal purchase amount: "))
+# if Total_purchase > 1000:
+#     discount= 0.1     
+# elif Total_purchase > 500:
+#     discount = 0.05
+# else:
+#     discount = 0
+# final_price = Total_purchase*(1-discount)
+# print(f"the total amount is : {Total_purchase}")
+# print(f"the total purchase discount is  :  {discount}")
+# print(f"the final price is {final_price}")
+
+
+# Total_purchase = float(input("Enter your toatal purchase amount: "))
+# if Total_purchase > 1000:
+#     discount= 10/100*(Total_purchase)  
+# elif Total_purchase > 500:
+#     discount = 5/100*(Total_purchase) 
+# else:
+#     discount = 0
+# final_price = Total_purchase-discount
+# print(f"the total amount is : {Total_purchase}")
+# print(f"the total purchase discount is  :  {discount}")
+# print(f"the final price is {final_price}")
+
+### this program is for the loan eligibility
+# age = int(input("Enter your age : "))
+# income = int(input("Enter your income : "))
+# credit_score = int(input("Enter your Credit_score : "))
+# if age >= 18 and income >= 30000 and credit_score >= 600:
+#     print("Your are eligible for the loan")
+# else:
+#     print("Your are not eligible for the loan")
+
+# x, y, z = "orange", "apple", "banana"
+
+# print(x,y,z)
+# print(x+y+z)
+
+
+# name, age, is_student = "Alice", 30, True
+# print(name)
+# print(age)
+# print(is_student)
+
+# x=y=z = "orange"
+# print(x,y,z)
+
+# imag_number = 3 + 4j
+
+# print(type(imag_number), imag_number)
+# txt = "Hello WORLD, pyhton, cpp"
+# print (txt[-7 : 4: 2])
+# print (txt.upper())
+# lower
+# title
+# capitalise
+# COUNT("l")
+# find("l")
+# replace("hello","asdf")
+# print(txt.split(" "))
+
+# list = ["Hello", "WORLD", "pyhton", "cpp"]
+# # new_lst = " ".join(list)
+# # print (new_lst)
+# print(" ".join(list))
+# sub = "Python"
+# print (sub[::-1])
